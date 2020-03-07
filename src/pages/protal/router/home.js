041217c0e43f4890082fd8b506f78227f3df945a
zhangliGit <674937076@qq.com>
2020-03-07 21:52:30 +0800

    新增疫情模块

---
+++ src/pages/protal/router/home.js
@@ -0,0 +1,24 @@
+/**
+ * @des 主页模块路由
+ * @key {module} 模块名
+ * @key {path} vue组件
+ * @key {title} 路由标题
+ * @author zhangli
+ */
+
+const Home = [
+  {
+    module: 'home',
+    path: 'Home',
+    type: 'index',
+    keepAlive: true,
+    title: '首页'
+  },
+  {
+    module: 'home',
+    path: 'Detail',
+    title: '详情'
+  }
+]
+
+export default Home
