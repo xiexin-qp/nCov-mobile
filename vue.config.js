b26332a73a62fae314d79e4a03de8fbe03eb1683
zhangliGit <674937076@qq.com>
2020-03-26 20:56:18 +0800

    配置优化

--- vue.config.js
+++ vue.config.js
@@ -65,7 +65,8 @@ module.exports = {
     hotOnly: false,
     proxy: {
       '/wangxuanzhang': {
-        target: 'http://wxz-test-001.natapp1.cc', // 王选章
+        target: 'http://39.97.164.4:9001/', // 外网
+        // target: 'http://wxz-test-001.natapp1.cc', // 王选章
         changeOrigin: true,
         ws: true,
         pathRewrite: {
@@ -73,7 +74,8 @@ module.exports = {
         }
       },
       '/zhuxu': {
-        target: 'http://s44kfw.natappfree.cc', // 朱旭
+        target: 'http://39.97.164.4:9001/', // 外网
+        // target: 'http://s44kfw.natappfree.cc', // 朱旭
         changeOrigin: true,
         ws: true,
         pathRewrite: {
